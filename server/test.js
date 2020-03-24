a00ab2b85ef83cfb29ddd13d62a439e1aab2c0f2
Louesilv <59611944+Louesilv@users.noreply.github.com>
2020-03-24 17:24:09 +0100

    test: post and get method

--- server/test.js
+++ server/test.js
@@ -15,12 +15,31 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended: true}));
 
 app.listen(9292, ()=>{
-    MongoClient.connect(CONNECTION_URL,{useNewUrlParser: true},(error,client)=>{
+    MongoClient.connect(CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology:true},(error,client)=>{
         if(error){
             throw error;
         }
         database=client.db(DATABASE_NAME);
         collection=database.collection("movies");
         console.log("Connected to '"+DATABASE_NAME+ "'!" );
+    });
+});
+
+
+app.post("/movies",(request,response)=>{
+    collection.insert(request.body,(error,result)=>{
+        if(error){
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+app.get("/movies",(request,response)=>{
+    collection.find({}).toArray((error,result)=>{
+        if(error){
+            return response.status(500).send(error);
+        }
+        response.send(result);
     });
 });
